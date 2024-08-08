efacbdcc4c5a88364a9af5af41793c77bac0333d
Juan Usme <jfusme@gmail.com>
2024-08-08 02:01:38 -0500

    "Removed unnecessary columns, added new tables and relationships, and modified existing tables and indexes in the database schema."

--- DB/ControlAgenda.sql
+++ DB/ControlAgenda.sql
@@ -8,21 +8,16 @@ CREATE TABLE AccesoUsuarios (
     IdAccesoUsuario INT IDENTITY(1,1),
     CorreoElectronico VARCHAR(80) UNIQUE NOT NULL,
     ContrasenaHash VARCHAR(255) NOT NULL,
-    FechaCreacion DATETIME DEFAULT GETDATE(),
-    FechaModificacion DATETIME DEFAULT GETDATE(),
     PRIMARY KEY (IdAccesoUsuario)
 );
 GO
 
 CREATE TABLE PerfilUsuarios (
     IdPerfilUsuario INT IDENTITY(1,1),
-    
     Nombre VARCHAR(60) NOT NULL,
     Apellidos VARCHAR(60) NOT NULL,
     Telefono VARCHAR(20) NOT NULL,
     IdAccesoUsuario INT NOT NULL,
-    FechaCreacion DATETIME DEFAULT GETDATE(),
-    FechaModificacion DATETIME DEFAULT GETDATE(),
     PRIMARY KEY (IdPerfilUsuario)
 );
 GO
@@ -30,32 +25,41 @@ GO
 CREATE TABLE SedesDeFormacion (
     IdSedeFormacion INT IDENTITY(1,1),
     Nombre VARCHAR(150) NOT NULL,
-    FechaCreacion DATETIME DEFAULT GETDATE(),
-    FechaModificacion DATETIME DEFAULT GETDATE(),
     PRIMARY KEY (IdSedeFormacion)
 );
 GO
+
+CREATE TABLE EtapaFormacion(
+    IdEtapaFormacion INT IDENTITY(1,1),
+    EtapaFormacion VARCHAR(45) NOT NULL,
+    PRIMARY KEY (IdEtapaFormacion)
+);
+GO;
+
+CREATE TABLE JornadaFormacion(
+    IdJornadaFormacion INT IDENTITY(1,1),
+    Jornada VARCHAR(15) NOT NULL,
+    PRIMARY KEY (IdJornada)
+)
 
 CREATE TABLE ProgramasDeFormacion (
     IdPrograma INT IDENTITY(1,1),
     ProgramaDeFormacion VARCHAR(80) NOT NULL,
     FechaCreacion DATETIME DEFAULT GETDATE(),
-    FechaModificacion DATETIME DEFAULT GETDATE(),
     PRIMARY KEY (IdPrograma)
 );
 GO
 
 CREATE TABLE FichasFormacion (
     NumeroFicha INT IDENTITY(1,1),
-    LugarFormacion VARCHAR(100),
-    FechaInicioFormacion DATE,
-    FechaInicioProductiva DATE,
-    FechaFinalizacionFormacion DATE,
-    JornadaFormacion VARCHAR(15),
     IdPrograma INT NOT NULL,
+    IdJornadaFormacion INT NOT NULL,
     IdSedeFormacion INT NOT NULL,
+    IdEtapaFormacion INT NOT NULL,
+    FechaInicioFormacion DATE NOT NULL,
+    FechaInicioProductiva DATE NOT NULL,
+    FechaFinalizacionFormacion DATE NOT NULL,
     FechaCreacion DATETIME DEFAULT GETDATE(),
-    FechaModificacion DATETIME DEFAULT GETDATE(),
     PRIMARY KEY (NumeroFicha)
 );
 GO
@@ -64,8 +68,6 @@ CREATE TABLE FichasFormacion_PerfilUsuarios (
     NumeroFicha INT,
     IdPerfilUsuario INT,
     EsLider TINYINT,
-    FechaCreacion DATETIME DEFAULT GETDATE(),
-    FechaModificacion DATETIME DEFAULT GETDATE(),
     PRIMARY KEY (NumeroFicha, IdPerfilUsuario)
 );
 GO
@@ -74,7 +76,6 @@ CREATE TABLE Calendario (
     IdCalendario INT IDENTITY(1,1),
     NumeroFicha INT NOT NULL,
     FechaCreacion DATETIME DEFAULT GETDATE(),
-    FechaModificacion DATETIME DEFAULT GETDATE(),
     PRIMARY KEY (IdCalendario)
 );
 GO
@@ -88,6 +89,21 @@ GO
 
 CREATE TABLE Eventos (
     IdEvento INT IDENTITY(1,1),
+    Competencia TEXT NOT NULL,
+    ResultadoAprendizaje TEXT NOT NULL,
+    Anotacion TEXT NOT NULL,
+    FechaInicio DATETIME NOT NULL,
+    FechaFin DATETIME NOT NULL,
+    IdEstadoEvento INT NOT NULL,
+    IdCalendario INT NOT NULL,
+    FechaCreacion DATETIME DEFAULT GETDATE(),
+    PRIMARY KEY (IdEvento)
+);
+GO
+
+CREATE TABLE EventosArchivados (
+    IdEventoArchivado INT IDENTITY(1,1),
+    IdEventoOriginal INT NOT NULL,
     Competencia VARCHAR(200) NOT NULL,
     ResultadoAprendizaje VARCHAR(200) NOT NULL,
     Anotacion VARCHAR(200) NOT NULL,
@@ -95,11 +111,10 @@ CREATE TABLE Eventos (
     FechaFin DATETIME NOT NULL,
     IdEstadoEvento INT NOT NULL,
     IdCalendario INT NOT NULL,
-    FechaCreacion DATETIME DEFAULT GETDATE(),
-    FechaModificacion DATETIME DEFAULT GETDATE(),
-    PRIMARY KEY (IdEvento)
+    FechaArchivado DATETIME DEFAULT GETDATE(),
+    PRIMARY KEY (IdEventoArchivado)
 );
-GO
+
 
 -- Foreign Keys
 ALTER TABLE PerfilUsuarios
@@ -111,7 +126,11 @@ ALTER TABLE FichasFormacion
 ADD CONSTRAINT FK_FichasFormacion_ProgramasDeFormacion 
 FOREIGN KEY (IdPrograma) REFERENCES ProgramasDeFormacion(IdPrograma),
 CONSTRAINT FK_FichasFormacion_SedesDeFormacion 
-FOREIGN KEY (IdSedeFormacion) REFERENCES SedesDeFormacion(IdSedeFormacion);
+FOREIGN KEY (IdSedeFormacion) REFERENCES SedesDeFormacion(IdSedeFormacion),
+CONSTRAINT Fk_FichasFormacion_JornadaFormacion
+FOREIGN KEY (IdJornadaFormacion) REFERENCES JornadaFormacion(IdJornadaFormacion),
+CONSTRAINT Fk_FichasFormacion_EtapaFormacion
+FOREIGN KEY (IdEtapaFormacion) REFERENCES EtapaFormacion(IdEtapaFormacion);
 GO
 
 ALTER TABLE FichasFormacion_PerfilUsuarios
@@ -133,24 +152,35 @@ CONSTRAINT FK_Eventos_EstadosEvento
 FOREIGN KEY (IdEstadoEvento) REFERENCES EstadosEvento(IdEstadoEvento);
 GO
 
+ALTER TABLE EventosArchivados
+ADD CONSTRAINT FK_EventosArchivados_EstadosEvento 
+FOREIGN KEY (IdEstadoEvento) REFERENCES EstadosEvento(IdEstadoEvento),
+CONSTRAINT FK_EventosArchivados_Calendario 
+FOREIGN KEY (IdCalendario) REFERENCES Calendario(IdCalendario);;
+
 -- Índices para optimizar las consultas
 CREATE INDEX idx_FichasFormacion_PerfilUsuarios_NumeroFicha ON FichasFormacion_PerfilUsuarios(NumeroFicha);
 CREATE INDEX idx_FichasFormacion_PerfilUsuarios_IdPerfilUsuario ON FichasFormacion_PerfilUsuarios(IdPerfilUsuario);
 CREATE INDEX idx_Eventos_IdCalendario ON Eventos(IdCalendario);
 CREATE INDEX idx_Eventos_FechaInicio ON Eventos(FechaInicio);
+CREATE INDEX idx_EventosArchivados_FechaInicio ON EventosArchivados(FechaInicio);
+CREATE INDEX idx_EventosArchivados_IdEventoOriginal ON EventosArchivados(IdEventoOriginal);
+
 GO
 
 -- Trigger
-CREATE TRIGGER create_calendar_after_insert_course
-AFTER INSERT ON cursos
+CREATE TRIGGER create_calendar_after_insert_ficha
+AFTER INSERT ON FichasFormacion
 FOR EACH ROW
 BEGIN
-  CALL create_calendar_for_course(NEW.id);
+  CALL create_calendar_for_ficha(NEW.NumeroFicha);
 END;
 
 -- Procedimiento almacenado
-CREATE PROCEDURE create_calendar_for_course(IN course_id INT)
+CREATE PROCEDURE create_calendar_for_ficha(IN ficha_id INT)
 BEGIN
-  INSERT INTO calendarios (curso_id) VALUES (course_id);
+  INSERT INTO Calendario (NumeroFicha) VALUES (NEW.ficha_id);
   -- Enviar notificación al frontend (por ejemplo, usando un evento en un sistema de mensajes)
 END;
+END;
+GO
